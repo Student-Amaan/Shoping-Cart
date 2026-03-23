0308bb8bdb55d60e4a3bd72929f90129651cdc68
waxir <danishdarman06@gmail.com>
2026-03-23 20:38:41 +0500

    product show in display

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const product = [
+  const products = [
     { id: 1, name: "spicefood", price: 50.99 },
     { id: 2, name: "simplefood", price: 20.99 },
     { id: 3, name: "specialfood", price: 70.99 },
   ];
 
+  let saveCart = [];
   const productList = document.getElementById("product-list");
   const cardItems = document.getElementById("cart-items");
   const emptyCard = document.getElementById("empty-cart");
@@ -12,7 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const totalPrice = document.getElementById("total-price");
   const checkOutBtn = document.getElementById("checkout-btn");
 
-  let saveCart = [];
-
-  
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+    <span>${product.name} - $${product.price.toFixed(1)}</span>
+    <button data-id="${product.id}">Add to cart</button>
+    `;
+    productList.appendChild(productDiv);
+  });
 });
