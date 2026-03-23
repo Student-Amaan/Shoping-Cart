c1cac7234753ac2cbd032fc3e4017faf5300215f
waxir <danishdarman06@gmail.com>
2026-03-23 20:13:14 +0500

    cart id is selected

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const product = [
+    { id: 1, name: "spicefood", price: 50.99 },
+    { id: 2, name: "simplefood", price: 20.99 },
+    { id: 3, name: "specialfood", price: 70.99 },
+  ];
+
+  const productList = document.getElementById("product-list");
+  const cardItems = document.getElementById("cart-items");
+  const emptyCard = document.getElementById("empty-cart");
+  const cardTotal = document.getElementById("cart-total");
+  const totalPrice = document.getElementById("total-price");
+  const checkOutBtn = document.getElementById("checkout-btn");
+
+  let saveCart = [];
+
+  
+});
